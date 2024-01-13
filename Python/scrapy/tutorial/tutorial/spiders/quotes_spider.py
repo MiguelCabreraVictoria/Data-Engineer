494c66f2af1b9057761f7786cd503aa0af5e445f
Miguel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2024-01-13 13:13:53 -0600

    Extracting Data

--- Python/scrapy/tutorial/tutorial/spiders/quotes_spider.py
+++ Python/scrapy/tutorial/tutorial/spiders/quotes_spider.py
@@ -10,27 +10,32 @@ Spiders are classes that you define and that Scrapy uses to scrape information f
 class QuotesSpider(scrapy.Spider):
     name = "quotes" #indetifies the Spider. it must be unique within a project
 
-"""
-must return an iterable of Requests ( you can return a list of request or write a generator function) which the Spider will begin to crawl from. Subsequent requests will be generated successively from these initial request
-"""
-    def start_requests(self):
-        urls = [
+    start_urls = [
             "https://quotes.toscrape.com/page/1/",
-            "https://quotes.toscrape.com/page/2/",
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
-"""
+            "https://quotes.toscrape.com/page/2/",]
+    
+    
+    """
 A method that will be called to handle the response downloaded for each of the request made. The response parameter is an instance of TextResponse that holds the page content and has futher helpful methods to handle it
 
 Usually parses the response extracting the scraped data as dicts and salso finding new URL´s to follow and creating new request
-"""
+    """
+    
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"quotes-{page}.html"
-        Path(filename).write_bytes(response.body)
-        self.log(f"Saved file {filename}")
+        for quote in response.css("div.quote"):
+            yield {
+                "text": quote.css("span.text::text").get(),
+                "author": quote.css("small.author::text").get(),
+                "tags": quote.css("div.tags a.tag::text").getall(),
+            }
+
+"""
+must return an iterable of Requests ( you can return a list of request or write a generator function) which the Spider will begin to crawl from. Subsequent requests will be generated successively from these initial request
+"""
+
+
+
 
 
 #Run Code scrapy crawl quotes
+# scrapy crawl quotes -o quotes.json 
